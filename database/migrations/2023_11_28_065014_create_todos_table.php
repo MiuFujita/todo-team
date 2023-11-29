a87277bb93ce98fab5903b26d8d028df1f0c143f
akadoh4869 <hodakobe@gmail.com>
2023-11-29 15:22:40 +0900

    controller 作成しました

--- database/migrations/2023_11_28_065014_create_todos_table.php
+++ database/migrations/2023_11_28_065014_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodosTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateTodosTable extends Migration
      */
     public function up()
     {
-        Schema::create('todos', function (Blueprint $table) {
-            $table->id();
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->text('task');
+            $table->increments('id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
