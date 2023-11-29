3687a0cd343112321ef60fb0f6f0cd09b46a6cac
MiuFujita <149229087+MiuFujita@users.noreply.github.com>
2023-11-29 23:00:00 +0800

    共有ページの大体

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -5,8 +5,152 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset ('css/share.css') }}">
 </head>
 <body>
-    
+    <header>
+        <div class="header-text">
+            <div class="header-left">
+                <p>ToDoList</p>
+            </div>
+            <div class="header-right">
+                <p>
+                    {{-- <a href="{{ route ('mytodo') }}"> --}}
+                        mylist
+                    {{-- </a> --}}
+                </p>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="share-main">
+            Weekly Planner
+        </div>
+        <fieldset class="share-box">
+            <div class="monday">
+                <legend>Monday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="tuesday">
+                <legend>Tuesday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="wednesday">
+                <legend>Wednesday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="thursday">
+                <legend>Thursday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="fryday">
+                <legend>Fryday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="saturday">
+                <legend>Saturday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="sunday">
+                <legend>Sunday</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+            <div class="others">
+                <legend>Others</legend>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    あいうえお</span>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox"/>
+                  <span>
+                    {{-- <a href="{{  }}"></a> --}}
+                    かきくけこ</span>
+                </div>    
+            </div>
+
+        </fieldset>
+          
+    </main>
 </body>
 </html>
