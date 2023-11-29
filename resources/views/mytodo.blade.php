69332efbce0533a87370063025e48908c0dec092
MiuFujita <149229087+MiuFujita@users.noreply.github.com>
2023-11-29 14:46:27 +0800

    トップページの更新

--- resources/views/mytodo.blade.php
+++ resources/views/mytodo.blade.php
@@ -8,5 +8,30 @@
 </head>
 <body>
     <h1>my to do list</h1>
+    <div class="todo-wrapper">
+        @foreach ($todos as $todo)
+            <div class="todo-box">
+                <div>{{ $todo->todo }}</div>
+                {{-- <div class="destroy-btn">
+                    @if ($tweet->user_id == Auth::user()->id)  
+                        <form action="{{ route('destroy',[$tweet->id]) }}" method="post">
+                            @csrf
+                        <input type="submit" value="削除">
+                        </form>
+                    @endif
+                </div> --}}
+            </div>
+            {{-- <div style="padding: 10px 40px">
+                @if($tweet->likedBy(Auth::user())->count() > 0)
+                <a href="/likes/{{ $tweet->likedBy(Auth::user())->firstOrfail()->id }}"><i class="fas fa-heart-broken"></i></a>
+                @else
+                <a href="/tweets/{{ $tweet->id }}/likes"><i class="far fa-heart"></a></i>
+                @endif
+                {{ $tweet->likes->count() }}
+            </div> --}}
+        @endforeach
+        {{-- {{ $tweets->links() }} --}}
+    </div>
+
 </body>
 </html>
